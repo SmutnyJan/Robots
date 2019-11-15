dc66395cbe1e384e8c76a78887817b79132f8fff
Jan Smutný <jan.smutny@pslib.cz>
2019-11-15 08:57:18 +0100

    Add project files.

---
+++ Robots/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Robots
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
