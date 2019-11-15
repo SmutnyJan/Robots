cdae8000eb2e5e26803efbbc7397e58f2f141f60
Jan Smutný <jan.smutny@pslib.cz>
2019-11-15 09:14:09 +0100

    RoboticPart

---
+++ Robots/RoboticPart.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Robots
+{
+    abstract class RoboticPart
+    {
+        public virtual string StatusMessage { get { return "This is General Robotic Part."; } }
+        public double Consumption { get { return DeviceConsumption; } }
+        public virtual double DeviceConsumption { get; protected set; }
+    }
+}
